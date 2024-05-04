8a51ebd2a47a6ac388366dbc367e3b438b8d8d59
anonfedora <musaeleazar090@gmail.com>
2024-05-04 20:47:24 +0100

    General

---
+++ general/BaseBall.js
@@ -0,0 +1,23 @@
+const baseBall = ops => {
+    var result = [];
+    for (i = 0; i < ops.length; i++) {
+        if (ops[i] != "+" && ops[i] != "D" && ops[i] != "C") {
+            result.push(parseInt(ops[i]));
+        } else if (ops[i] == "+") {
+            result.push(
+                parseInt(result[result.length - 2]) +
+                    parseInt(result[result.length - 1])
+            );
+        } else if (ops[i] == "D") {
+            result.push(parseInt(result[result.length - 1]) * 2);
+        } else {
+            result.pop();
+        }
+    }
+    const total = result.reduce((curr, prev) => {
+        return curr + prev;
+    });
+    return total;
+};
+
+console.log(baseBall(["5", "2", "C", "D", "+"]));
