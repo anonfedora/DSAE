7ef1e4e57532178b1086f8e083c0ae9988895970
anonfedora <musaeleazar090@gmail.com>
2024-05-04 20:47:49 +0100

    General

---
+++ general/BaseBall1.js
@@ -0,0 +1,23 @@
+const baseball = ops => {
+    result = [];
+    for (i = 0; i < ops.length; i++) {
+        switch (ops[i]) {
+            case "+":
+                let value =
+                    result[result.length - 2] + result[result.length - 1];
+                result.push(value);
+                break;
+            case "D":
+                let value1 = result[result.length - 1] * 2;
+                result.push(value1);
+                break;
+            case "C":
+                result.pop();
+                break;
+            default:
+                result.push(parseInt(ops[i]));
+        }
+    }
+    return result.length ? result.reduce((curr, prev) => curr + prev) : 0;
+};
+console.log(baseball(["5", "2", "C", "D", "+"]));
