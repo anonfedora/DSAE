83e49120146870928582d396cb02b2446ec47106
anonfedora <musaeleazar090@gmail.com>
2024-05-04 13:50:22 +0100

    General

---
+++ general/MergeTwoLists.js
@@ -0,0 +1,13 @@
+const mergeTwoLists = (l1, l2) => {
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+};
+
+console.log(mergeTwoLists([1,3,3], [4,5,6]));
