3b6239e62936bfd3c199d87a9f66f510e2a347d2
anonfedora <musaeleazar090@gmail.com>
2024-05-10 15:55:28 +0100

    General

---
+++ general/list/DeleteAllOccurence.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor() {
+        this.data = 0;
+        this.next = null;
+    }
+}
+
+function push(head, data) {
+    var node = new Node();
+    node.data = data;
+    node.next = head;
+    head = node;
+    return head;
+}
+
+function printList(node) {
+    while (node.next != null) {
+        console.log(node.data + "->");
+        node = node.next;
+    }
+}
+
+function deleteKey(head, key) {
+    var temp = head;
+
+    while (temp.next != null) {
+        if (head.data == key) head = head.next;
+        if (head.next.data == key) {
+            head.next = head.next.next;
+        } else {
+            temp = temp.next;
+        }
+    }
+    return head
+}
