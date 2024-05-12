63c3cca926ad0f09fb45a6fe85ac616951fc1ffe
anonfedora <musaeleazar090@gmail.com>
2024-05-12 08:33:35 +0100

    General

--- general/MergeTwoLists.js
+++ general/MergeTwoLists.js
@@ -1,10 +1,28 @@
-class Node{
-  
+class Node {
+    constructor() {
+        this.data = 0;
+        this.next = null;
+    }
 }
+
+function push(head, data) {
+    var node = new Node();
+    node.data = data;
+    node.next = head;
+    head = node;
+    return head;
+}
+function printList(node) {
+    while (node != null) {
+        console.log("->", node.data);
+        node = node.next;
+    }
+}
+
 const mergeTwoLists = (l1, l2) => {
     if (!l1) return l2;
     else if (!l2) return l1;
-    else if (l1.val <= l2.val) {
+    else if (l1.data <= l2.data) {
         l1.next = mergeTwoLists(l1.next, l2);
         return l1;
     } else {
@@ -13,4 +31,23 @@ const mergeTwoLists = (l1, l2) => {
     }
 };
 
-console.log(mergeTwoLists([1,3,3], [4,5,6]));
+var l1 = null,
+    l2 = null;
+l1 = push(l1, 3);
+l1 = push(l1, 2);
+l1 = push(l1, 1);
+printList(l1);
+l2 = push(l2, 6);
+l2 = push(l2, 5);
+l2 = push(l2, 4);
+printList(l2);
+/*console.log("after merge");
+mergeTwoLists(l1, l2);
+console.log("l1");
+printList(l1);
+console.log("l2");
+printList(l2);*/
+mergeTwoLists(l2, l1)
+console.log('after merge')
+printList(l1)
+printList(l2)
