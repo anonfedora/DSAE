48359904df1c509599c9bb94e1368adff014802b
anonfedora <musaeleazar090@gmail.com>
2024-05-13 15:19:56 +0100

    General

---
+++ important/RemoveOccurence.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor() {
+        this.data = 0;
+        this.next = null;
+    }
+}
+
+const push = (head, data) => {
+    let node = new Node();
+    node.data = data;
+    node.next = head;
+    head = node;
+    return head;
+};
+
+const removeOccurence = (head, x) => {
+    let temp = head;
+    /* while (head.data === x) {
+        head = head.next;
+    }*/
+    while (temp.next != null) {
+        if (temp.data == x) {
+            //temp = temp.next;
+            return temp;
+        } else if (temp.next.data == x) {
+            temp.next = temp.next.next;
+            return temp.next
+        } else return temp;
+    }
+    return temp;
+};
+
+const printList = node => {
+    while (node != null) {
+        console.log(node.data + "->");
+        node = node.next;
+    }
+};
+
+let head = null;
+head = push(head, 5);
+head = push(head, 4);
+head = push(head, 3);
+head = push(head, 2);
+head = push(head, 1);
+head = push(head, 3);
+head = push(head, 3);
+
+printList(head);
+console.log("After Removal");
+head = removeOccurence(head, 3);
+printList(head);
