d7aa0f8265d4b653ecaf6b65dc31d71be9d5eb5b
anonfedora <musaeleazar090@gmail.com>
2024-05-21 22:02:45 +0100

    General

---
+++ important/ReverseLL.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const push = (head, data) => {
+    var node = new Node(data);
+    node.next = head;
+    head = node;
+    return node;
+};
+
+const printList = node => {
+    var str = "";
+    while (node != null) {
+        str += node.data + "->";
+        node = node.next;
+    }
+    console.log(str + "NULL");
+};
+
+const reverseList = head => {
+    var prev = null;
+    var curr = head;
+    var next = null;
+    while (curr != null) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    
+    return head;
+};
+
+var head = null;
+head = push(head, 5);
+head = push(head, 4);
+head = push(head, 3);
+head = push(head, 2);
+head = push(head, 1);
+printList(head);
+head = reverseList(head);
+printList(head);
