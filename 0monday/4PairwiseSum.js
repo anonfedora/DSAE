64ff0374babadbf524abf82cfa84cd06ab8ca59c
anonfedora <musaeleazar090@gmail.com>
2024-05-28 07:24:41 +0100

    List

---
+++ 0monday/4PairwiseSum.js
@@ -0,0 +1,62 @@
+class Node {
+    constructor() {
+        this.data = 0;
+        this.next = this.prev = null;
+    }
+}
+
+const printList = node => {
+    let str = "";
+    while (node != null) {
+        str += node.data + "->";
+        node = node.next;
+    }
+    console.log(str);
+};
+
+const push = (head, data) => {
+    let node = new Node();
+    node.data = data;
+    node.prev = node.next = null;
+    if (head == null) head = node;
+    else {
+        node.next = head;
+        head.prev = node;
+    }
+    head = node;
+    return head;
+};
+
+const pairwiseSum = (head, x) => {
+    let first = head;
+    let second = head;
+    let isFound = false;
+
+    while (second.next != null) second = second.next;
+
+    while (first != second && second.next != first) {
+        if (first.data + second.data == x) {
+            console.log("(" + first.data + " , " + second.data + ")");
+
+            first = first.next;
+            second = second.prev;
+        } else {
+            if (first.data + second.data < x) {
+                first = first.data;
+            } else {
+                second = second.prev;
+            }
+        }
+    }
+};
+let head = null;
+head = push(head, 6);
+head = push(head, 5);
+head = push(head, 4);
+head = push(head, 3);
+head = push(head, 2);
+head = push(head, 1);
+/*head = push(head, -1);
+head = push(head, -2);*/
+printList(head);
+pairwiseSum(head, 5);
