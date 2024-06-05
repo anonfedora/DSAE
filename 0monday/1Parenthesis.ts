b37fc92903a581b74321f546265c2c90d8c2450b
anonfedora <musaeleazar090@gmail.com>
2024-06-05 13:58:42 +0100

    Stack

---
+++ 0monday/1Parenthesis.ts
@@ -0,0 +1,12 @@
+/**
+ * function checking if a combination of parenthesis os valid
+ *
+ * This function defines the data structure and validation rules for handling a basic
+ * user query.
+ *
+ * @function isValid
+ *
+ * @property str - The string parenthesis combination provided by the user.
+ *                        It must be a non-empty string.
+ * return Boolean
+ */
