e16f0bec6eae79dec8758287f98906c00bc15e37
anonfedora <musaeleazar090@gmail.com>
2024-06-05 13:58:45 +0100

    Stack

--- 0monday/1Parenthesis.js
+++ 0monday/1Parenthesis.js
@@ -1,3 +1,16 @@
+/**
+ * function checking if a combination of parenthesis os valid
+ *
+ * This function defines the data structure and validation rules for handling a basic
+ * user query.
+ *
+ * @function isValid
+ *
+ * @property str - The string parenthesis combination provided by the user.
+ *                        It must be a non-empty string.
+ * return Boolean
+ */
+
 const isValid = str => {
     let stack = [];
     for (let s of str) {
