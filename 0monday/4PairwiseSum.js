22abf486144202f8acdca17d139b77991b21d31e
anonfedora <musaeleazar090@gmail.com>
2024-06-05 13:58:39 +0100

    List

--- 0monday/4PairwiseSum.js
+++ 0monday/4PairwiseSum.js
@@ -1,3 +1,17 @@
+/**
+ * class for swapping a pair of Linked List Nodes
+ *
+ * This function defines the data structure and validation rules for handling a basic
+ * user query.
+ *
+ * @class Node
+ * @function printList
+ * @function push
+ * @function pairwiseSwap
+ * @property head - First Node
+ *                        It must be a non-empty string.
+ * return swapped List
+ */
 class Node {
     constructor() {
         this.data = 0;
