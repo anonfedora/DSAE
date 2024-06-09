25fbf01c162044cd7efa721d2f49684a94aae761
anonfedora <musaeleazar090@gmail.com>
2024-06-09 16:14:40 +0100

    Recursion

---
+++ 0monday/5PalindromeRecursion.ts
@@ -0,0 +1,15 @@
+/**
+ * @Author Musa Eleazar Shekoaga
+ * class for swapping a pair of Linked List Nodes
+ *
+ * This function defines the data structure and validation rules for handling a basic
+ * user query.
+ *
+ * @class Node
+ * @function printList
+ * @function push
+ * @function pairwiseSwap
+ * @property head - First Node
+ *                        It must be a non-empty string.
+ * return swapped List
+ */
